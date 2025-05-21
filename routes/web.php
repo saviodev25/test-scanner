6d5d5cbb41b9ed0abbb95206fc0926ce5b50c560
saviodev25 <saviodev21@gmail.com>
2025-05-20 23:08:20 -0300

    teste commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\VendaController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return 'Funcionando';
+});
 Route::get('/{loja}', [VendaController::class, 'index']);
 Route::get('/produto/{codigo}', [VendaController::class, 'buscarProduto']);
 Route::post('/registrar/{loja}', [VendaController::class, 'registrar']);
